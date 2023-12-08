b32157fea8683e344a2b831252f07f6dc4b6a862
Noemi Ticona Angles <nticonaa@bg.com.bo>
2023-12-08 11:02:01 -0400

    test

---
+++ CheckInDomain/src/test/java/factories/PropiedadFactoryTest.java
@@ -0,0 +1,36 @@
+package factories;
+
+import core.BusinessRuleValidationException;
+import com.nur.factories.propiedad.PropiedadFactory;
+import com.nur.model.Propiedad;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+class PropiedadFactoryTest {
+   private PropiedadFactory checkInFactory;
+
+   @BeforeEach
+   void setUp() {
+      checkInFactory = new PropiedadFactory();
+   }
+
+   @Test
+   void testCreateCheckIn() throws BusinessRuleValidationException {
+
+      UUID id = UUID.randomUUID();
+      String nombre = "Sample Property";
+      String estado = "Available";
+      double precio = 1000.0;
+
+      Propiedad propiedad = checkInFactory.create(String.valueOf(id), nombre, estado, precio);
+
+      Assertions.assertNotNull(propiedad);
+      Assertions.assertEquals(id, propiedad.getId());
+      Assertions.assertEquals(nombre, propiedad.getNombre());
+   }
+
+
+}
