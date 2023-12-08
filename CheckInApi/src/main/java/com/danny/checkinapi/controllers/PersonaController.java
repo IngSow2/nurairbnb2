719f004a15682448bd18b6a1f15346728b81bba1
Noemi Ticona Angles <nticonaa@bg.com.bo>
2023-12-08 13:48:49 -0400

    modificacion test

--- CheckInApi/src/main/java/com/danny/checkinapi/controllers/PersonaController.java
+++ CheckInApi/src/main/java/com/danny/checkinapi/controllers/PersonaController.java
@@ -3,8 +3,6 @@ package com.danny.checkinapi.controllers;
 import an.awesome.pipelinr.Pipeline;
 import dtos.PersonaDto;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +12,7 @@ import use.cases.command.persona.get.persona.GetPersonaQuery;
 @RestController
 public class PersonaController {
 
-  Logger logger = LoggerFactory.getLogger(PersonaController.class);
+  //  Logger logger = LoggerFactory.getLogger(PersonaController.class);
   final Pipeline pipeline;
 
   public PersonaController(Pipeline pipeline) {
